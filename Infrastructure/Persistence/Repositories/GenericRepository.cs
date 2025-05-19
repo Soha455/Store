6dd2491e85cf449c51c610145bca307cef295888
SOHA <SOHA@LAPTOP-D0C015FV>
2025-05-18 20:32:06 -0800

    #RefactoringGenericClassRepository

--- Infrastructure/Persistence/Repositories/GenericRepository.cs
+++ Infrastructure/Persistence/Repositories/GenericRepository.cs
@@ -34,8 +34,6 @@ namespace Persistence.Repositories
             //if (trackChanges) return await _context.Set<TEntity>().ToListAsync();
             //return await _context.Set<TEntity>().AsNoTracking().ToListAsync();
         }
-
-
         public async Task<TEntity?> GetAsync(TKey id)
         {
             return await _context.Products.Include(P => P.ProductType).Include(P => P.ProductBrand).FirstOrDefaultAsync(P => P.Id == id as int?) as TEntity;
@@ -51,6 +49,21 @@ namespace Persistence.Repositories
         public void Delete(TEntity entity)
         {
             _context.Remove(entity);
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync(ISpecifications<TEntity, TKey> spec, bool trackChanges = false)
+        {
+            return await ApplySpecifications(spec).ToListAsync();
+        }
+
+        public async Task<TEntity?> GetAsync(ISpecifications<TEntity, TKey> spec, TKey id)
+        {
+            return await ApplySpecifications(spec).FirstOrDefaultAsync();
+        }
+
+        private IQueryable<TEntity> ApplySpecifications(ISpecifications<TEntity, TKey> spec) 
+        {
+            return SpecificationEvaluator.GetQuery(_context.Set<TEntity>(), spec);
         }
     }
 }
---
+++ Infrastructure/Persistence/SpecificationEvaluator.cs
@@ -0,0 +1,34 @@
+﻿using Domain.Contracts;
+using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence
+{
+    static class SpecificationEvaluator
+    {
+        // Generate Query Method
+        public static IQueryable<TEntity> GetQuery<TEntity, TKey>(
+                IQueryable<TEntity> inputQuery ,
+                ISpecifications<TEntity, TKey> spec 
+                )
+                where TEntity :BaseEntity<TKey>
+        {
+            var query = inputQuery;
+
+            // Where 
+            if (spec.Criteria is not null)
+                query = query.Where(spec.Criteria);
+
+            // Include
+            query = spec.IncludeExpressions.Aggregate(query, (currentQuery ,IncludeExpression) => currentQuery.Include(IncludeExpression));
+
+            return query;
+        }
+    }
+}
